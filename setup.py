3f6a7c98f41f6210cc17328ced24e8bfa4829aed
Will Usher <wusher@kth.se>
2020-06-12 10:10:47 +0200

    Parse author list in packaging script

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
 from setuptools import setup, find_packages
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+with open("AUTHORS", "r") as fh:
+    authors = []
+    text_authors = fh.readlines()
+    for author in text_authors:
+        authors.append(author.split("<")[0].strip())
+
 setup(
     name="Amply",
     version="0.1",
     packages=find_packages("src"),
+    license='Eclipse Public License 1.0 (EPL-1.0)',
 
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
@@ -11,24 +22,22 @@ setup(
     package_data={
         # If any package contains *.txt or *.rst files, include them:
         "": ["*.txt", "*.rst"],
-        # And include any *.msg files found in the "hello" package, too:
-        "hello": ["*.msg"],
     },
 
     # metadata to display on PyPI
-    author="Q. Lim, W. Usher",
+    author=",".join(authors),
     author_email="wusher@kth.se",
-    description="Amply allows you to load and manipulate AMPL data as Python data structures",
+    description="Amply allows you to load and manipulate AMPL/GLPK data as Python data structures",
+    long_description=long_description,
+    long_description_type="text/x-rst",
     keywords="ampl gmpl",
-    url="http://github.com/coin-or/amply",   # project home page, if any
+    url="http://github.com/willu47/amply",   # project home page, if any
     project_urls={
-        "Bug Tracker": "http://github.com/coin-or/amply/issues",
-        "Documentation": "http://github.com/coin-or/amply/README.rst",
-        "Source Code": "http://github.com/coin-or/amply",
+        "Bug Tracker": "http://github.com/willu47/amply/issues",
+        "Documentation": "http://github.com/willu47/amply/README.rst",
+        "Source Code": "http://github.com/willu47/amply",
     },
     classifiers=[
-        "License :: OSI Approved :: Python Software Foundation License"
+        "License :: OSI Approved :: Eclipse Public License 1.0 (EPL-1.0)"
     ]
-
-    # could also include long_description, download_url, etc.
 )
